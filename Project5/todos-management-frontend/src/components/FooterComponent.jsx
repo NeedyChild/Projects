d4c7d4ca055662f5a499d8f96ed3daf43ec3c3dc
NeedyChild <100189185+NeedyChild@users.noreply.github.com>
2023-12-27 17:46:19 -0800

    Add files via upload

---
+++ Project5/todos-management-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <div>
+            <footer className='footer'>
+                <p className='text-center'>Copyrights reserved at 2024</p>
+            </footer>
+        </div>
+    </div>
+  )
+}
+
+export default FooterComponent
